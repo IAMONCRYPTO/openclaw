6ac5d47c0b0587d4a85658484b8d06fb5ffb055f
IAMONCRYPTO <onwebp@gmail.com>
2026-04-25 19:49:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { execSync } = require("child_process");
 console.log("Installing OpenClaw...");
 execSync("npm install -g openclaw@latest", { stdio: "inherit" });
 
-console.log("Starting OpenClaw gateway...");
-execSync("openclaw gateway start", { stdio: "inherit" });
+console.log("Starting OpenClaw gateway directly...");
+execSync("openclaw gateway --no-daemon", { stdio: "inherit" });
